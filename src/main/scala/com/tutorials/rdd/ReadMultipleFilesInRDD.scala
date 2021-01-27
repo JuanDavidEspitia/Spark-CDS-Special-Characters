23817bafda38b182a74a79a4b42d6d17d6694d60
Juan David Espitia <juandespitia14@gmail.com>
2021-01-27 14:50:55 -0500

    Read multiple text files into a single RDD with comma separator

--- src/main/scala/com/tutorials/rdd/ReadMultipleFilesInRDD.scala
+++ src/main/scala/com/tutorials/rdd/ReadMultipleFilesInRDD.scala
@@ -5,8 +5,7 @@ import org.apache.spark.sql.SparkSession
 
 object ReadMultipleFilesInRDD
 {
-  def main(args: Array[String]): Unit =
-  {
+  def main(args: Array[String]): Unit = {
     /**
      * Inicilizamos los Logger que solo muestre por consola ERROR y WARM
      * */
@@ -16,7 +15,7 @@ object ReadMultipleFilesInRDD
     /**
      * Iniciamos la Session de Saprk
      * */
-    val spark:SparkSession = SparkSession.builder()
+    val spark: SparkSession = SparkSession.builder()
       .master("local[1]")
       .appName("SparkByExamples.com")
       .getOrCreate()
@@ -24,11 +23,45 @@ object ReadMultipleFilesInRDD
     /**
      * Spark Leer todos los archivos de texto de un directorio en un solo RDD
      * */
-    val textFilePath ="/input/texfiles_rdd/*"
-    val rdd = spark.sparkContext.textFile(textFilePath)
-    rdd.foreach(f=>{
+    val rdd = spark.sparkContext.textFile("input/textfiles_rdd/*")
+    rdd.foreach(f => {
       println(f)
     })
+
+    /**
+     * Podemos usar la funcion Collect y Foreach para imprimir el contenido del RDD
+     * */
+    println("Otra forma de imprimir el contenido del RDD")
+    rdd.collect.foreach(f=>{
+      println(f)
+    })
+
+    /**
+     * El método wholeTextFiles (). devuelve un RDD [Tuple2].
+     * donde el primer valor (_1) en una tupla es un nombre de archivo y
+     * el segundo valor (_2) es el contenido del archivo.
+     * */
+    println("Imprimimos con el metodo wholeTextFiles (). Devuelve una tupla \n" +
+      "NombreRuta_Archivo, Contenido_Archivo")
+    val rddWhole = spark.sparkContext.wholeTextFiles("input/textfiles_rdd/*")
+    println("                              NombreRuta_Archivo                                            |           Contenido_Archivo     ")
+    rddWhole.foreach(f=>{
+      println(f._1+" --> "+f._2)
+    })
+
+    /**
+     * Leer multiples archivos de texto en un solo RDD
+     * */
+
+
+
+
+
+
+
+
+
+
 
 
 
