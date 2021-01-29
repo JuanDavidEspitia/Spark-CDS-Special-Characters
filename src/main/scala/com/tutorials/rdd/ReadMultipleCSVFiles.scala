e5e52f9e779accfd9cc1f66be90da33eb745a971
Juan David Espitia <juandespitia14@gmail.com>
2021-01-29 14:18:00 -0500

    Create multiple files csv with content

---
+++ src/main/scala/com/tutorials/rdd/ReadMultipleCSVFiles.scala
@@ -0,0 +1,23 @@
+package com.tutorials.rdd
+
+import org.apache.log4j.{Level, Logger}
+
+object ReadMultipleCSVFiles
+{
+  def main(args: Array[String]): Unit =
+  {
+    /**
+     * Inicilizamos los Logger que solo muestre por consola ERROR y WARM
+     * */
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("spark").setLevel(Level.WARN)
+
+    
+
+
+
+
+
+  }
+
+}
